2560584a239fea8707352748a706a94e00b4c0b8
José Carlos Duque Filho <cj_duque@hotmail.com>
2020-12-14 14:29:49 -0300

    return origin

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -16,6 +16,6 @@ namespace SalesWebMvc {
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-               
+                .UseStartup<Startup>();
     }
 }
